e5bb68f858955f2dfac6541fd12c89d87e273670
Mizanur Rahman RIyadh <mizanurriyadh@ymail.com>
2019-05-10 11:45:05 -0700

    Array Rotation in python using list

---
+++ RotateArray.py
@@ -0,0 +1,23 @@
+def Rotation(array, D):
+    array = array[D:] + array[:D]
+    return array
+
+
+def Print(array):
+    for i in array:
+        print(i, end=" ")
+    print()
+
+
+def main():
+    T = int(input())
+    while (T):
+        T -= 1
+        N, D = list(map(int, input().split()))
+        array = list(map(int, input().split()))
+        new_array = Rotation(array, D)
+        Print(new_array)
+
+
+if __name__ == '__main__':
+    main()
